7ccc403a139866b7fa0a4c05c895c482af820722
KashanAdnan-Coder <computertechhub0@gmail.com>
2024-10-24 08:43:00 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+// let container = document.querySelector("#container")
+// container.style.backgroundColor = "blue"
+// container.style.color = "white"
+// // container childNodes
+// // container children
+// let countChild = container.childNodes
+// // .length
+// console.log(countChild.length)
+// // console.log(container.nodeName)
+
+// let container = document.querySelector("#container")
+
+// let count = container.childNodes
+// console.log(count.length)
+
+// let button = document.querySelector("button")
+
+// function changeMe() {
+//     button.setAttribute("class", "btn")
+//     button.removeAttribute("class")
+//     console.log(button.hasAttribute("class"));
+//     // true
+// }
+
+function openPopup() {
+    let wasay = document.querySelector(".popup-container")
+    wasay.style.transform = "scale(100%) translate(80%, 100%)"
+}
